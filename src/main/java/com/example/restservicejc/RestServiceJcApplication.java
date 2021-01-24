f0576953604f2bdacfbed4630334c9e50c532f22
iky1981 <iky@chughtais.co.uk>
2021-01-24 14:54:06 +0000

    first commit

---
+++ src/main/java/com/example/restservicejc/RestServiceJcApplication.java
@@ -0,0 +1,13 @@
+package com.example.restservicejc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceJcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceJcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restservicejc/RestServiceJcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restservicejc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServiceJcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
